ccfad2458e5ca88ee9b87f77dc41d4662ed6c9d4
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-01-22 01:04:40 -0300

    import libraries

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List, Dict
 
 from pydantic import BaseModel
 from os import getenv
@@ -8,9 +8,9 @@ load_dotenv()
 
 class Settings(BaseModel):
     TOKEN: Optional[str] = getenv("TOKEN")
-    PREFIX: Optional[str] = getenv("PREFIX")
     POSTGRES_PASSWORD: Optional[str] = getenv("POSTGRES_PASSWORD")
     POSTGRES_DSN: Optional[str] = getenv("POSTGRES_DSN")
+    HOURLY_EARNINGS: Optional[float] = float(getenv("HOURLY_EARNINGS", 20.0))
 
 
 env = Settings()
---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .play_audio import PlayAudioEffect
