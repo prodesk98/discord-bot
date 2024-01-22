5f5c15cd6f870ce25dd7639663ab086d04c3ec59
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-01-22 13:54:06 -0300

    main updated

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ from config import env
 from loguru import logger
 
 from commands import (
-    CoinsCommand
+    CoinsCommand,
+    InstructCommand,
+    AskingCommand,
+    QuizCommand
 )
+from utils import has_bot_manager_permissions
 
 intents = Intents.default()
 
@@ -33,12 +37,77 @@ class Bot(commands.Bot):
 bot = Bot()
 
 @bot.tree.command(
+    name="quiz",
+    description="Criar um Quiz"
+)
+@app_commands.checks.cooldown(1, 60, key=lambda i: (i.guild_id, i.user.id))
+async def quiz(interaction: Interaction, tema: str):
+    if not has_bot_manager_permissions(interaction.user.roles):
+        raise Exception("Você não tem permissão para criar um quiz.")
+    if env.LEARN_BOT_ENDPOINT is None or env.LEARN_BOT_AUTHORIZATION is None:
+        raise Exception("API não foi configurado corretamente.")
+    if not env.LEARN_BOT_ENABLED:
+        raise Exception("API desativada")
+
+    await interaction.response.defer(ephemeral=False)
+    embed = await QuizCommand(interaction, tema)
+
+
+
+@bot.tree.command(
+    name="asking",
+    description="Perguntar ao robô"
+)
+@app_commands.checks.cooldown(1, 30.0, key=lambda i: (i.guild_id, i.user.id))
+async def asking(interaction: Interaction, pergunta: str):
+    if env.LEARN_BOT_ENDPOINT is None or env.LEARN_BOT_AUTHORIZATION is None:
+        raise Exception("API não foi configurado corretamente.")
+    if not env.LEARN_BOT_ENABLED:
+        raise Exception("API desativada")
+
+    await interaction.response.defer(ephemeral=False)
+
+    embed = await AskingCommand(interaction, pergunta)
+    if embed is None:
+        return
+    await interaction.edit_original_response(embed=embed)
+
+
+@bot.tree.command(
+    name="instruct",
+    description="Ensinar ao robô"
+)
+@app_commands.checks.cooldown(1, 10.0, key=lambda i: (i.guild_id, i.user.id))
+async def instruct(interaction: Interaction, texto: str):
+    if not has_bot_manager_permissions(interaction.user.roles):
+        raise Exception("Você não tem permissão para ensinar o robô.")
+    if env.LEARN_BOT_ENDPOINT is None or env.LEARN_BOT_AUTHORIZATION is None:
+        raise Exception("API não foi configurado corretamente.")
+    if not env.LEARN_BOT_ENABLED:
+        raise Exception("API desativada")
+
+    text_size = len(texto)
+    if text_size < 65:
+        raise Exception("Texto muito pequeno, é necessário no mínimo 65 caracteres.")
+    elif text_size > 4000:
+        raise Exception("Texto excedeu o limite máximo de 4000 caracteres.")
+
+    await interaction.response.defer(ephemeral=True)
+
+    embed, file = await InstructCommand(interaction, texto)
+    if embed is None:
+        return
+    await interaction.edit_original_response(embed=embed, content="", attachments=[file])
+
+@bot.tree.command(
     name="ping",
-    description="Server latency"
+    description="Latência do servidor"
 )
 @app_commands.checks.cooldown(1, 5.0, key=lambda i: (i.guild_id, i.user.id))
-@commands.has_guild_permissions()
 async def ping(interaction: Interaction):
+    if not has_bot_manager_permissions(interaction.user.roles):
+        raise Exception("Você não tem permissão para executar esse comando.")
+
     embed = Embed(
         title="🏓 Pong!",
         description=f"The bot latency is {round(interaction.client.latency * 1000)}ms.",
@@ -48,16 +117,18 @@ async def ping(interaction: Interaction):
 
 @bot.tree.command(
     name="coins",
-    description="Show account balance"
+    description="Mostrar saldo da conta"
 )
 @app_commands.checks.cooldown(2, 15, key=lambda i: (i.guild_id, i.user.id))
 async def coins(interaction: Interaction):
-    await interaction.response.send_message("buscando saldo...", ephemeral=True)
+    await interaction.response.defer(ephemeral=True)
     embed, file = await CoinsCommand(interaction)
-    await interaction.edit_original_response(embed=embed, attachments=[file])
+    await interaction.edit_original_response(embed=embed, content="", attachments=[file])
 
 @ping.error
 @coins.error
+@instruct.error
+@asking.error
 async def on_error(interaction: Interaction, error: app_commands.AppCommandError):
     if isinstance(error, app_commands.CommandOnCooldown):
         embed = Embed(
@@ -66,11 +137,14 @@ async def on_error(interaction: Interaction, error: app_commands.AppCommandError
             color=0xE02B2B
         )
         await interaction.response.send_message(embed=embed, ephemeral=True)
-    return Embed(
-        title="Error!",
+        return
+
+    embed = Embed(
+        title="❌ Error!",
         description=f"{error}",
         color=0xE02B2B
     )
+    await interaction.response.send_message(embed=embed, ephemeral=True)
 
 
 bot.run(env.TOKEN)
