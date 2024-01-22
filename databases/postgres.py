2f85ca067776be2e14d9e74570ec95d137c7a7ca
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-01-22 01:04:23 -0300

    async postgres

--- databases/postgres.py
+++ databases/postgres.py
@@ -1,22 +1,56 @@
-from sqlalchemy.orm import sessionmaker, declarative_base
-from typing import Generator
-from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs, AsyncSession
+from sqlalchemy import (
+    Column, Integer, String,
+    Boolean, Float, DateTime,
+    Sequence, func, JSON
+)
 from config import env
 
-engine = create_engine(
+engine = create_async_engine(
     env.POSTGRES_DSN,
-    pool_pre_ping=True,
-    pool_recycle=3600,
-    pool_size=20,
-    max_overflow=0
+    echo=True
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
-Base = declarative_base()
+async_session = AsyncSession(
+    engine,
+    expire_on_commit=False,
+)
 
-def get_session() -> Generator:
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, Sequence("users_id_seq"), primary_key=True)
+    discord_user_id = Column(String(32))
+    discord_username = Column(String(50))
+    discord_nick = Column(String(50))
+    received_initial_coins = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+
+class CoinsHistory(Base):
+    __tablename__ = 'coins_history'
+
+    id = Column(Integer, Sequence("coins_history_id_seq"), primary_key=True)
+    user_id = Column(Integer)
+    amount = Column(Float)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+
+class Quizzes(Base):
+    __tablename__ = 'quizzes'
+
+    id = Column(Integer, Sequence("quizzes_id_seq"), primary_key=True)
+    status = Column(Integer)
+    theme = Column(String(100))
+    question = Column(String(100))
+    alternatives = Column(JSON)
+    truth = Column(Integer)
+    voice_url = Column(String(1000), nullable=True, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
