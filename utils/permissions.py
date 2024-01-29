00ddbaeee280fbcd304dcfde94f32a12ced1dcdc
prodesk98 <133387879+prodesk98@users.noreply.github.com>
2024-01-29 04:11:25 -0300

    v0.0.6

--- utils/permissions.py
+++ utils/permissions.py
@@ -9,8 +9,8 @@ from database import AsyncDatabaseSession, User
 def has_bot_manager_permissions(roles: List[Role]):
     return env.BOT_MANAGE_ROLE in [role.name for role in roles]
 
-async def has_account(user_id: int) -> bool:
+async def has_account(discord_user_id: int) -> bool:
     async with AsyncDatabaseSession as session:
         return normalize_value(
             (await session.execute(
-            select(func.count(User.id).label("count")).where(User.discord_user_id == str(user_id)))).scalar()) > 0  # type: ignore
+            select(func.count(User.id).label("count")).where(User.discord_user_id == str(discord_user_id)))).scalar()) > 0  # type: ignore
